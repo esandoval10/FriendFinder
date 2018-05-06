c0e32c5b1bc3024be68c52195916413f96e8e54d
Edgar Sandoval <ESE1307@Edgars-Air.attlocal.net>
2018-05-06 03:35:36 -0500

    Works

---
+++ routing/apiRoutes.js
@@ -0,0 +1,45 @@
+
+var friends = require("../app/data/friends");
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+    
+    app.post("/api/friends", function(req, res) {
+        
+        var beffos = {
+            name: "",
+            photo: "",
+            matchScore: Infinity
+        };
+
+        var userData = req.body;
+        var userScores = userData.scores;
+        var diff;
+
+        for(var i = 0; i < friends.length; i++){
+            var current = friends[i];
+            diff = 0;
+            console.log(current.name);
+
+            for(var s = 0; s < current.scores.length; s++){
+                var currentScore = current.scores[s];
+                var currentUser = userScores[s];
+
+                diff = Math.abs(parseInt(currentUser) - parseInt(currentScore));
+            }
+
+            if (diff <= beffos.friendDifference) {
+                beffos.name = current.name;
+                beffos.photo = current.photo;
+                beffos.matchScore = diff;
+            }
+        }
+        friends.push(userData);
+
+        res.json(beffos);
+
+    });
+
+}
